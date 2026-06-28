080ee78881a50e160c95c8c103ea5924472c6a32
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2026-06-28 21:12:26 +0300

    define PlayerClass enum class

--- src/main/java/main/model/PlayerClass.java
+++ src/main/java/main/model/PlayerClass.java
@@ -2,6 +2,20 @@ package main.model;
 
 public enum PlayerClass {
 
-    // TODO: Complete enumeration
+    STORM_SCREAMER("Storm Screamer"),
+    MOONLIGHT_SENTINEL("Moonlight Sentinel"),
+    PHOENIX_KNIGHT("Phoenix Knight"),
+    TRICKSTER("Trickster"),
+    DREADNOUGHT("Dreadnought");
+
+    private final String displayName;
+
+    PlayerClass(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 
 }
